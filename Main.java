d66a6b826a76cdc7c7eefcdf1d47d8ae0164e7e4
nmurphy95 <nolan.murphy95@gmail.com>
2019-02-07 21:25:36 +0100

    Added initial project files

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+
+public class Main {
+	
+	@FXML
+	private Button btBlue;
+	
+	@FXML
+	protected void blueButtonPressed(ActionEvent e) {
+		
+	}
+}
