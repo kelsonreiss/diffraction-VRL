3cfe1b0cfd8a5e3aed33f69196f3882e13810998
nmurphy95 <nolan.murphy95@gmail.com>
2019-02-27 22:03:04 +0100

    Updated GUI

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,30 +1,189 @@
 package application;
 
+import java.text.DecimalFormat;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.*;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.event.ActionEvent;
 
 public class Controller {
 	@FXML
 	private Button btSeparation, btWidth, btDistance;
 	@FXML
+	private ToggleGroup colorBts, slitBts;
+	@FXML
 	private RadioButton btRed, btBlue, btGreen, btSingle, btDouble;
 	@FXML
 	private Slider slSeparation, slWidth, slDistance;
-
-	private String currentColor;
-
-	public String getCurrentColor() {
-		return currentColor;
+	@FXML
+	private NumberAxis chtX, chtY;
+	@FXML
+	private LineChart<Number, Number> chtIntensity;
+	@FXML
+	private TextField txtSeparation, txtWidth, txtDistance;
+	
+	@FXML
+	public void initialize() {
+		slSeparation.valueProperty().addListener((observable, oldValue, newValue) -> {
+			separationChangedSlider();
+		});
+		
+		slWidth.valueProperty().addListener((observable, oldValue, newValue) -> {
+			widthChangedSlider();
+		});
+		
+		slDistance.valueProperty().addListener((observable, oldValue, newValue) -> {
+			distanceChangedSlider();
+		});
 	}
+	
+	
 
-	public void setCurrentColor(String currentColor) {
-		this.currentColor = currentColor;
+	private String selectedColor, slitType;
+	private double selectedSeparation, selectedWidth, selectedDistance;
+	private int txtChangeType;
+	
+	private DecimalFormat df = new DecimalFormat(".#");
+	
+	final double SEP_MAX = 10;
+	final double SEP_MIN = 0;
+	final double WID_MAX = 3;
+	final double WID_MIN = 0.5;
+	final double DIS_MAX = 1;
+	final double DIS_MIN = 0.5;
+	/*
+	 * Updates display with the chosen color
+	 * 
+	 */
+	@FXML
+	protected void colorChanged(ActionEvent e) {
+		RadioButton selectedRadioButton = (RadioButton) colorBts.getSelectedToggle();
+		selectedColor = selectedRadioButton.getText();
+		simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
 	}
+	
+	@FXML
+	protected void slitChanged(ActionEvent e) {
+		RadioButton selectedRadioButton = (RadioButton) slitBts.getSelectedToggle();
+		slitType = selectedRadioButton.getText();
+		simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+	}
+	
 
-	protected void colorPressed() {
-
+	
+	protected void separationChangedSlider() {
+		selectedSeparation = slSeparation.getValue();
+		txtSeparation.setText(df.format(selectedSeparation));
+		
+		//Add in functionality to tie value of input field to "OK" button, and dynamically adjust slider with change
+		simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+	}
+	
+	@FXML
+	protected void separationChangedText(ActionEvent e) {
+		txtChangeType = 1;
+		String tempSeparation = txtSeparation.getText();
+		if (inputValidator(tempSeparation, txtChangeType)) {
+			selectedSeparation = Double.valueOf(tempSeparation);
+			slSeparation.setValue(selectedSeparation);
+			simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+		}
+		else {
+			txtSeparation.setText(Double.toString(selectedSeparation));
+		}
+	}
+	
+	protected void widthChangedSlider() {
+		selectedWidth = slWidth.getValue();
+		txtWidth.setText(df.format(selectedWidth));
+		
+		//Add in functionality to tie value of input field to "OK" button, and dynamically adjust slider with change
+		simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+	}
+	
+	@FXML
+	protected void widthChangedText(ActionEvent e) {
+		txtChangeType = 2;
+		String tempWidth = txtWidth.getText();
+		if (inputValidator(tempWidth, txtChangeType)) {
+			selectedWidth = Double.valueOf(tempWidth);
+			slWidth.setValue(selectedWidth);
+			simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+		}
+		else {
+			txtWidth.setText(Double.toString(selectedWidth));
+		}
+		
+	}
+	
+	protected void distanceChangedSlider() {
+		selectedDistance = slDistance.getValue();
+		txtDistance.setText(df.format(selectedDistance));
+		
+		//Add in functionality to tie value of input field to "OK" button, and dynamically adjust slider with change
+		simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+	}
+	
+	@FXML
+	protected void distanceChangedText(ActionEvent e) {
+		txtChangeType = 3;
+		String tempDistance = txtDistance.getText();
+		if (inputValidator(tempDistance, txtChangeType)) {
+			selectedDistance = Double.valueOf(tempDistance);
+			slDistance.setValue(selectedDistance);
+			simulate(selectedColor, slitType, selectedSeparation, selectedWidth, selectedDistance);
+		}
+		else {
+			txtDistance.setText(Double.toString(selectedDistance));
+		}
+	}
+	
+	protected boolean inputValidator(String input, int valueType) {
+		try {
+			double doubInput = Double.valueOf(input);
+			
+			if (valueType == 1) {
+				if (SEP_MIN <= doubInput && doubInput <= SEP_MAX) {
+					return true;
+				}
+			}
+				
+			if (valueType == 2) {
+				if (WID_MIN <= doubInput && doubInput <= WID_MAX) {
+					return true;
+				}
+			}
+			
+			if (valueType == 3) {
+				if (DIS_MIN <= doubInput && doubInput <= DIS_MAX) {
+					return true;
+				}
+							}
+		}
+		catch(Exception e) {
+			return false;
+		}
+		return false;
+	}
+	
+	/*
+	 * Populates the intensity graph
+	 * 
+	 */
+	protected void populateIntensity() {
+		
+	}
+	
+	/*
+	 * Updates the simulation including aperture, intensity graph, and diffraction pattern
+	 * 
+	 */
+	protected void simulate(String color, String slit, double separation, double width, double distance) {
+		// Add graphic simulator functions in Aperture interface and do the same for Diffraction/Intensity class... or put all in one class
 	}
 	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-
+		
+		//Set maximum and minimum bounds to fixedSize
+		int fixedSize = 800;
+		primaryStage.setMinHeight(fixedSize + 13);
+		primaryStage.setMaxHeight(fixedSize + 13);
+		primaryStage.setMinWidth(fixedSize);
+		primaryStage.setMaxWidth(fixedSize);
+		primaryStage.setTitle("Optics Virtual Lab");
+		
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
